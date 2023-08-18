203eec2e619bacf624bdc52eecd995a30bae88db
2023ITBRandharsan <142487516+2023ITBRandharsan@users.noreply.github.com>
2023-08-18 08:40:06 +0000

    Done1

---
+++ Hellowordex.java
@@ -0,0 +1,16 @@
+public class Helloword
+{
+    public static viod main(Stringagrs[])
+    {
+        System.out.println("Hello, WORLD");
+        System.out.println("Hello, WORLD");
+        System.out.println("Hello, WORLD");
+        System.out.println("Hello, WORLD");
+        System.out.println("Hello, WORLD");
+        System.out.println("Hello, WORLD");
+        System.out.println("Hello, WORLD");
+        System.out.println("Hello, WORLD");
+        System.out.println("Hello, WORLD");
+        System.out.println("Hello, WORLD");
+    }
+}
